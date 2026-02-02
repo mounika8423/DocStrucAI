a310ceda007ec73b6990995c0dc9582c1f4c2e17
Lakshmi <nlmounika8@gmail.com>
2026-02-03 03:53:40 +0530

    first step of model integration with ai

---
+++ app1.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv(override=True)
+key=os.getenv("OPENAI_API_KEY")
+
+message="Hi, This is my first project using AI"
+messages=[{"role":"user","content":message}]
+openai=OpenAI()
+response=openai.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=messages
+)
+response=openai.chat.completions.create(model="gpt-5-nano",messages=messages)
+print(response)
+system_prompt="""
+You are a helpful assistant that helps in 
+finding values from a document when a schema is given.
+You will be given a schema and a document."""
